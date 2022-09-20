84a6ed7cc7f9e9ece233938cab2b2335c79eb996
lee <2068046150@qq.com>
2022-09-20 16:32:51 +0800

    master commit_push

--- src/main/java/com/lzl/GitTest.java
+++ src/main/java/com/lzl/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("test4");
         System.out.println("hot-fix test5");
         System.out.println("master test5");
+        System.out.println("push test");
     }
 }
